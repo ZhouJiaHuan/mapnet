f52fd5924c21f9cbe6728ccdf46a6afdcf7f127f
ZhouJH <z_jiahuan@outlook.com>
2020-08-05 09:53:21 +0800

    add backbone resnet50, add configure files.

--- mapnet/models/__init__.py
+++ mapnet/models/__init__.py
@@ -1,12 +1,12 @@
 from .posenet import PoseNet, MapNet, AtLoc, AtLocPlus
-from .backbone import ResNet34
+from .backbone import ResNet18, ResNet34, ResNet50
 from .criterion import QuaternionLoss, PoseNetCriterion, MapNetCriterion
 from .optimizer import Optimizer
 
 from .posenet_for_cpp import PoseNetCpp, MapNetCpp
 
 __all__ = ['PoseNet', 'MapNet', 'AtLoc', 'AtLocPlus',
-           'ResNet34', 'QuaternionLoss', 'PoseNetCriterion',
-           'MapNetCriterion', 'Optimizer']
+           'ResNet18', 'ResNet34', 'ResNet50', 'QuaternionLoss',
+           'PoseNetCriterion', 'MapNetCriterion', 'Optimizer']
 
 __all__ += ['PoseNetCpp', 'MapNetCpp']
--- mapnet/models/backbone.py
+++ mapnet/models/backbone.py
@@ -4,12 +4,33 @@ from torchvision import models
 from ..registry import BACKBONES
 
 
+@BACKBONES.register_module(name='ResNet18')
+class ResNet18(nn.Module):
+
+    def __init__(self, pretrained):
+        super(ResNet18, self).__init__()
+        self.model = models.resnet18(pretrained=pretrained)
+
+    def forward(self, x):
+        return self.model(x)
+
 @BACKBONES.register_module(name='ResNet34')
 class ResNet34(nn.Module):
 
     def __init__(self, pretrained):
         super(ResNet34, self).__init__()
         self.model = models.resnet34(pretrained=pretrained)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+@BACKBONES.register_module(name='ResNet50')
+class ResNet50(nn.Module):
+
+    def __init__(self, pretrained):
+        super(ResNet50, self).__init__()
+        self.model = models.resnet50(pretrained=pretrained)
 
     def forward(self, x):
         return self.model(x)
