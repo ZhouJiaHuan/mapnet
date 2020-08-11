ce6fac7070c428cf9dd5f026957d720ee9ff235f
ZhouJH <z_jiahuan@outlook.com>
2020-08-11 10:52:56 +0800

    modify test codes

--- test_codes/test_backbone.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('.')
-# from mapnet.models import ResNet34
-from mapnet.builder import build_backbone
-
-
-cfgs = {'type': 'ResNet34', 'pretrained': True}
-resnet34 = build_backbone(cfgs)
-print(resnet34)
